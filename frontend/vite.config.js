af174a9dadbc11a9630928f6884aa477c58c0aa8
Piyush Singh <piyush061618@gmail.com>
2026-01-05 17:43:53 +0530

    Change API proxy target to production URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api": {
-         target: "http://localhost:5000"
+         target: "https://note-app-hl37.onrender.com/"
       }
     }
   }
